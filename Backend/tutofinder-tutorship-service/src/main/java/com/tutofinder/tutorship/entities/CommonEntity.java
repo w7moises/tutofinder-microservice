43421de6cbc47a69c024ddb8b4856c597d7da831
Moises Molina <w7moises@gmail.com>
2021-06-06 16:45:38 -0500

    auditing-common-class

---
+++ Backend/tutofinder-tutorship-service/src/main/java/com/tutofinder/tutorship/entities/CommonEntity.java
@@ -0,0 +1,25 @@
+package com.tutofinder.tutorship.entities;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class CommonEntity implements Serializable{
+    @Column(name = "created_date")
+    @CreatedDate
+    private Date createdDate;
+
+    @Column(name = "last_update_date")
+    @LastModifiedDate
+    private Date lastUpdateDate;
+}
