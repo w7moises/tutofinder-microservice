8a9d15cde897724685e7a2a6bf2edf96e57686b2
Moises Molina <w7moises@gmail.com>
2021-06-06 15:48:51 -0500

    template for customer

---
+++ Backend/tutofinder-customer-service/src/main/java/com/tutofinder/customer/TutofinderCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tutofinder.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutofinderCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutofinderCustomerServiceApplication.class, args);
+	}
+
+}
---
+++ Backend/tutofinder-customer-service/src/test/java/com/tutofinder/customer/TutofinderCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutofinder.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutofinderCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
