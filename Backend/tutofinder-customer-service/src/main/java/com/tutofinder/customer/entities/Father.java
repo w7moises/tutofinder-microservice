70d30cf4985da07dc28eb37cf264209cd6a4ed7b
Moises Molina <w7moises@gmail.com>
2021-06-07 12:33:43 -0500

    father entity added

---
+++ Backend/tutofinder-customer-service/src/main/java/com/tutofinder/customer/entities/Father.java
@@ -0,0 +1,43 @@
+package com.tutofinder.customer.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "fathers")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Father extends CommonEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(unique = true,nullable = false,length = 8)
+    private String dni;
+
+    @Email(message = "@ required")
+    @Column(unique = true,nullable = false)
+    private String email;
+
+    @Lob
+    @JsonIgnore
+    @Column(name = "profile_picture")
+    private byte[] profilePicture;
+
+}
