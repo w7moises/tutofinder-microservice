aca8e59aede8270a2f273ffc98273ee80e937bbc
Moises Molina <w7moises@gmail.com>
2021-06-08 15:00:15 -0500

    create teacher dto added

--- Backend/tutofinder-customer-service/src/main/java/com/tutofinder/customer/dto/create/CreateTeacherDto.java
+++ Backend/tutofinder-customer-service/src/main/java/com/tutofinder/customer/dto/create/CreateTeacherDto.java
@@ -1,21 +1,16 @@
-package com.tutofinder.customer.dto;
+package com.tutofinder.customer.dto.create;
 
+import com.tutofinder.customer.dto.AddressDto;
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
-public class TeacherDto {
-    private Long id;
+public class CreateTeacherDto {
     private String firstName;
     private String lastName;
     private String dni;
     private String email;
     private String depositNumber;
     private Double hourlyCost;
-    private Boolean membership;
     private byte[] profilePicture;
     private AddressDto address;
-    private Date createdDate;
-    private Date lastUpdateDate;
 }
