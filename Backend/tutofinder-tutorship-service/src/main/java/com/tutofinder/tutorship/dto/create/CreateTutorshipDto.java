909d99de37960e585747a0a13e19b5cdcc11cba0
William <wmavalle@gmail.com>
2021-06-10 18:25:12 -0500

    dtos

--- Backend/tutofinder-tutorship-service/src/main/java/com/tutofinder/tutorship/dto/create/CreateTutorshipDto.java
+++ Backend/tutofinder-tutorship-service/src/main/java/com/tutofinder/tutorship/dto/create/CreateTutorshipDto.java
@@ -1,4 +1,18 @@
 package com.tutofinder.tutorship.dto.create;
 
+import com.tutofinder.tutorship.entities.Course;
+import com.tutofinder.tutorship.entities.Report;
+import lombok.Data;
+
+@Data
 public class CreateTutorshipDto {
+    private int minutesAmmount;
+
+    private String tutorShipDescription;
+
+    private Course course;
+
+    private Report report;
+
+    private String status;
 }
