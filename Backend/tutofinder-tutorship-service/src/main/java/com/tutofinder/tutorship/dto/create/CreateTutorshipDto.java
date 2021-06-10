67156f451e0b33a613e47dd072deeec82bb71a1d
William <wmavalle@gmail.com>
2021-06-10 18:24:23 -0500

    respositories

--- Backend/tutofinder-tutorship-service/src/main/java/com/tutofinder/tutorship/dto/create/CreateTutorshipDto.java
+++ Backend/tutofinder-tutorship-service/src/main/java/com/tutofinder/tutorship/dto/create/CreateTutorshipDto.java
@@ -1,20 +1,4 @@
 package com.tutofinder.tutorship.dto.create;
 
-import com.tutofinder.tutorship.entities.Course;
-import com.tutofinder.tutorship.entities.Report;
-import lombok.Data;
-
-@Data
-
-public class CreateTutorShipDto {
-    private int minutesAmmount;
-
-    private String tutorShipDescription;
-
-    private Course course;
-
-    private Report report;
-
-    private String status;
-
+public class CreateTutorshipDto {
 }
--- Backend/tutofinder-tutorship-service/src/main/java/com/tutofinder/tutorship/repositories/TutorshipRepository.java
+++ Backend/tutofinder-tutorship-service/src/main/java/com/tutofinder/tutorship/repositories/TutorshipRepository.java
@@ -1,4 +1,15 @@
 package com.tutofinder.tutorship.repositories;
 
-public class TutorshipRepository {
+import com.tutofinder.tutorship.entities.Tutorship;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TutorshipRepository extends JpaRepository<Tutorship,Long> {
+    Optional<Tutorship> findById(Long id);
+
+    List<Tutorship> findAll();
 }
