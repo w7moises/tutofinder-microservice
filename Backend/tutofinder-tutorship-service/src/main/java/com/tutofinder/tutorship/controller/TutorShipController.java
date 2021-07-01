fab61650a9b0a0e76f96a3dc2a26c0c5c52cb1c3
William <wmavalle@gmail.com>
2021-07-01 16:24:36 -0500

    tutorship fixes

--- Backend/tutofinder-tutorship-service/src/main/java/com/tutofinder/tutorship/controller/TutorShipController.java
+++ Backend/tutofinder-tutorship-service/src/main/java/com/tutofinder/tutorship/controller/TutorShipController.java
@@ -14,13 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -59,7 +53,7 @@ public class TutorShipController {
 
     @ApiOperation(value = "Creates a tutorShip", notes = "This Operation creates a new tutorShip.")
     @PostMapping(value = "tutorShip")
-    public ResponseEntity<TutorShipDto> createTutorShip(@Valid CreateTutorShipDto tutorShipDto) {
+    public ResponseEntity<TutorShipDto> createTutorShip(@RequestBody @Valid CreateTutorShipDto tutorShipDto) {
         TutorShip tutorShip = tutorShipService.createTutorShip(tutorShipDto);
         return new ResponseEntity<>(converter.convertTutorShipToDto(tutorShip), HttpStatus.CREATED);
     }
